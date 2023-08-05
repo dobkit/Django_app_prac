5dbcce650a4db3a054a38e0e1ef217d1e3fcea86
dobkit <dopham.bkit@gmail.com>
2023-08-05 12:23:00 -0400

    show exam result 2

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 # <HINT> Import any new Models here
 from .models import Course, Enrollment, Question, Choice, Submission
-from .models import Course, Enrollment
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
@@ -133,18 +132,23 @@ def submit(request, course_id):
     #     choice = get_object_or_404(Choise, pk=choice_id)
     #     submission.choice.add(Choise.objects.get)
 
-    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(submission.id,)))
+    return HttpResponseRedirect(reverse(viewname='onlinecourse:show_exam_result', args=(course_id, submission.id,)))
 
 
 # <HINT> A example method to collect the selected choices from the exam form from the request object
 def extract_answers(request):
-   submitted_anwsers = []
-   for key in request.POST:
-       if key.startswith('choice'):
-           value = request.POST[key]
-           choice_id = int(value)
-           submitted_anwsers.append(choice_id)
-   return submitted_anwsers
+    submitted_answers = []
+    choice_ids = request.POST.getlist('selected_choices[]')
+    for choice_id in choice_ids:
+        submitted_answers.append(int(choice_id))
+    return submitted_answers
+    # submitted_anwsers = []
+    # for key in request.POST:
+    #     if key.startswith('choice'):
+    #         value = request.POST[key]
+    #         choice_id = int(value)
+    #         submitted_anwsers.append(choice_id)
+    # return submitted_anwsers
 
 
 # <HINT> Create an exam result view to check if learner passed exam and show their question results and result for each question,
